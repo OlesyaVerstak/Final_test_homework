7901cb0772eeedde35c7656e3d6f0bbf8ff5a297
Olesya <o.verstak99@gmail.com>
2023-01-24 16:33:52 +0700

    done

---
+++ task1/Program.cs
@@ -0,0 +1,28 @@
+﻿// Написать программу которая из имеющегося массива строк формируют массив из строк длина которых меньше либо равна 3 символа. 
+string[] incomingArray;
+Console.WriteLine("Введите массив строк через пробел");
+string stroka = Console.ReadLine();
+incomingArray = stroka.Split(' ');
+string[] outgoingArray = new string[incomingArray.Length];
+void SecondArrayWithIF(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (array1[i].Length <= 3)
+        {
+            array2[count] = array1[i];
+            count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+SecondArrayWithIF(incomingArray, outgoingArray);
+PrintArray(outgoingArray);
